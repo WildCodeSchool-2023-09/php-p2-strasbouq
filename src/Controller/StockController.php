c7bd61d01c3f2e1f9988e43c84b709416a8418e8
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2023-11-14 17:37:46 +0100

    ajout de la page message admin

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -9,7 +9,7 @@ class StockController extends AbstractController
 {
     public function index(): string
     {
-        $stockManager = new StockManager(); 
+        $stockManager = new StockManager();
         $stocks = $stockManager->selectAll();
 
         return $this->twig->render('Admin/stock-form.html.twig', ['stocks' => $stocks]);
@@ -20,7 +20,7 @@ class StockController extends AbstractController
         $stockManager = new StockManager();
         if (!empty($_POST)) {
             $stockManager->update($_POST);
-            
+
             header('Location:/stock');
         }
 
@@ -33,11 +33,10 @@ class StockController extends AbstractController
     {
         $stockManager = new StockManager();
         if (!empty($_POST)) {
-
             $stockManager->increment($_POST);
             header('Location:/stock');
         }
-        
+
         $restocks = $stockManager->selectOneById($id);
 
         return $this->twig->render('Admin/stock-restock.html.twig', ['restocks' => $restocks]);
