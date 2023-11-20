d051696ee87d908f59389a3348701a71f348acb0
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2023-11-20 09:54:25 +0100

    ajout de la page bouquet personnalise

--- src/Controller/PersonalBouquetController.php
+++ src/Controller/PersonalBouquetController.php
@@ -7,6 +7,7 @@ class PersonalBouquetController extends AbstractController
     public function personalBouquet(): string
     {
         if (!empty($_POST)) {
+            var_dump($_POST);
         }
 
         return $this->twig->render('Home/personal_bouquet.html.twig');
